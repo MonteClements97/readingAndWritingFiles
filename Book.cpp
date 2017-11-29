d8c748c8f64991a9a46587e8d53de3699e3d04ff
Montell Clements <moc17@aber.ac.uk>
2017-11-29 21:12:21 +0000

    Playing with vectors

--- Book.cpp
+++ Book.cpp
@@ -1,7 +1,7 @@
 //
 // Created by moc17 on 23/11/17.
 //
-
+#include <vector>
 #include "Book.h"
 
 Book::Book() {
@@ -14,7 +14,9 @@ Book::~Book() {
 int main(){
     int array_index = 0;
     Book *books [11];
+    vector<Book*> vector_books;
     ifstream file_in;
+    ofstream file_out;
     file_in.open("../biblio.txt");
     if (file_in.is_open()){
         while(file_in.good()){
@@ -22,6 +24,27 @@ int main(){
             this_book->read_file(&file_in);
             books[array_index++] = this_book;
         }
+    }
+    file_in.close();
+    file_out.open("../biblio_with_semi.txt");
+    for(Book * book : books){
+        book->write_file_semi_colon(&file_out);
+    }
+    file_out.close();
+    file_in.open("../biblio_with_semi.txt");
+    if (file_in.is_open()){
+        while(file_in.good()) {
+            Book *this_book = new Book;
+            this_book->read_file_semi_colon(&file_in);
+            if (this_book->isbn == 0){
+                continue;
+            }
+            vector_books.push_back(this_book);
+        }
+    }
+    vector<Book*>::const_iterator it;
+    for(it = vector_books.begin(); it != vector_books.end(); ++it){
+        (*it) -> print();
     }
 
     return 0;
--- Book.h
+++ Book.h
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <istream>
 
 using namespace std;
 #ifndef WORKSHEETFILESIO_BOOK_H
@@ -14,7 +15,10 @@ public:
     int isbn;
     string ignore;
 
+    void const print();
     void read_file(ifstream *file);
+    void write_file_semi_colon(ofstream *file);
+    void read_file_semi_colon(ifstream *file);
     Book();
     ~Book();
 
@@ -24,13 +28,39 @@ public:
 
 void Book::read_file( ifstream *file ){
 
-        getline(*file, author);
-        getline(*file, title);
-        *file >> isbn;
-        getline(*file, ignore);
-        getline(*file, ignore);
+    getline(*file, author);
+    getline(*file, title);
+    *file >> isbn;
+    getline(*file, ignore);
+    getline(*file, ignore);
+    author.erase(author.size() - 1);
+    title.erase(title.size() - 1);
 
 }
+
+void Book::write_file_semi_colon ( ofstream *file) {
+    *file << author + ";" + title + ";" + to_string(isbn) + ";";
+}
+
+
+void Book::read_file_semi_colon (ifstream *file){
+    char first_author[100];
+    char first_title[100];
+    char first_isbn[11];
+    file->getline(first_author, 256, ';');
+    file->getline(first_title, 256, ';');
+    file->getline(first_isbn, 11, ';');
+    author = first_author;
+    title = first_title;
+    isbn = atoi(first_isbn);
+
+}
+
+void const Book::print(){
+    cout << author << ' ' << title << ' ' << isbn << ' ' << endl;
+}
+
+
 
 
 #endif //WORKSHEETFILESIO_BOOK_H
